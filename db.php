507df2e1656f2e9642b95e26d21524c1b1e8e5c2
lalaina <lalainaarandriamiandrisoa@gmail.com>
2022-12-17 09:08:49 +0300

    arborescence

--- db.php
+++ db.php
@@ -2,133 +2,131 @@
 
 class Database
 {
-    private $db_host = "sql206.epizy.com";
-    private $db_user = "epiz_30607380";
-    private $db_pass = "q6EC0DWsZWcbFs";
-    private $db_name = "epiz_30607380_categorie";
+    private $db_host = "https://testdes191662com:3zYHUlD@wf-shared-001.webo-facto.com/mysql/";
+    private $db_user = "testdes191686com";
+    private $db_pass = "FEYAElDe";
+    private $db_name = "dbtestdevphplalainas191686com";
     private $result = [];
     private $conn = false;
     private $mysqli = "";
 
     public function __construct()
     {
-        if(!$this->conn){
-            $this->mysqli = new mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
-            if($this->mysqli->connect_error){
-                array_push($this->result,$this->mysqli->connect_error);
+        if (!$this->conn) {
+            $this->mysqli = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
+            if ($this->mysqli->connect_error) {
+                array_push($this->result, $this->mysqli->connect_error);
                 return false;
             }
-        }
-        else{
+        } else {
             return true;
         }
     }
 
     public function insert(string $table, array $data)
     {
-        if($this->tableExists($table)){
-            $table_column = implode(', ',array_keys($data));
+        if ($this->tableExists($table)) {
+            $table_column = implode(', ', array_keys($data));
             $valeur = [];
-            foreach($data as $da){
-                $valeur[] = '\''.$da.'\'';  
+            foreach ($data as $da) {
+                $valeur[] = '\'' . $da . '\'';
             }
-            $val = implode(',',$valeur);
+            $val = implode(',', $valeur);
 
             $sql = "INSERT INTO $table ($table_column) VALUES ($val)";
-            if($this->mysqli->query($sql)){
+            if ($this->mysqli->query($sql)) {
                 $this->result[] = $this->mysqli->insert_id;
                 return true;
-            }else{
+            } else {
                 $this->result[] = $this->mysqli->error;
                 return false;
             }
-        }else{
+        } else {
             return false;
         }
     }
 
     public function update(string $table, array $data, string $where = null)
     {
-        if($this->tableExists($table)){
+        if ($this->tableExists($table)) {
             $args = [];
             foreach ($data as $k => $dt) {
-                 $args[] = "$k = '$dt'";
+                $args[] = "$k = '$dt'";
             }
-            $val = implode(',',$args);
+            $val = implode(',', $args);
             $sql = "UPDATE $table SET $val";
-            if($where != null){
+            if ($where != null) {
                 $sql .= " WHERE $where";
             }
-            if($this->mysqli->query($sql)){
+            if ($this->mysqli->query($sql)) {
                 $this->result[] = "modifier avec success";
                 return true;
-            }else{
+            } else {
                 $this->result[] = $this->mysqli->error;
                 return false;
             }
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function delete(string $table,string $where = null)
+    public function delete(string $table, string $where = null)
     {
-        if($this->tableExists($table)){
+        if ($this->tableExists($table)) {
             $sql = "DELETE FROM $table";
-            if($where != null){
+            if ($where != null) {
                 $sql .= " WHERE $where";
             }
-            if($this->mysqli->query($sql)){
+            if ($this->mysqli->query($sql)) {
                 $this->result[] = "supprimer avec success";
                 return true;
-            }else{
+            } else {
                 $this->result[] = $this->mysqli->error;
                 return false;
             }
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function select(string $table,string $rows = "*" , string $join = null,string $where = null,string $order = null,string $limit = null,$joi = null)
+    public function select(string $table, string $rows = "*", string $join = null, string $where = null, string $order = null, string $limit = null, $joi = null)
     {
-        if($this->tableExists($table)){
+        if ($this->tableExists($table)) {
             $sql = "SELECT $rows FROM $table";
-            if($join != null){
+            if ($join != null) {
                 $sql .= " JOIN $join";
             }
-            if($where != null){
+            if ($where != null) {
                 $sql .= " WHERE $where";
             }
-            if($order != null){
+            if ($order != null) {
                 $sql .= " ORDER BY $order";
             }
-            if($limit != null){
+            if ($limit != null) {
                 $sql .= " LIMIT  0,$limit";
             }
-            if($joi != null){
+            if ($joi != null) {
                 $sql .= "LEFT JOIN  $joi";
             }
 
             $query = $this->mysqli->query($sql);
-            if($query){
+            if ($query) {
                 return $query->fetch_all(MYSQLI_ASSOC);
-            }else{
+            } else {
                 $this->result[] = $this->mysqli->error;
                 return false;
             }
-            
-        }else{
+        } else {
             return false;
         }
-    } 
+    }
 
     public function sql(string $sql)
     {
         $query = $this->mysqli->query($sql);
-        if($query){
+        if ($query) {
             $this->result = $query->fetch_all(MYSQLI_ASSOC);
-        }else{
+        } else {
             $this->result[] = $this->mysqli->error;
             return false;
         }
@@ -138,7 +136,7 @@ class Database
     {
         $sql = "SHOW TABLES FROM $this->db_name LIKE '$table'";
         $tableInDb = $this->mysqli->query($sql);
-        if($tableInDb->num_rows == 1){
+        if ($tableInDb->num_rows == 1) {
             return true;
         }
         $this->result[] = " la table : $table n'existe pas";
@@ -151,7 +149,4 @@ class Database
         $this->result = [];
         return $val;
     }
-
-
-
 }
